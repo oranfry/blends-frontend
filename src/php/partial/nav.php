f360b0032047f64265cf3c032812cc27bf8f852f
Oran Fry <oran@emerg.co>
2020-04-15 16:35:57 +1200

    Implement explicit nav

--- src/php/partial/nav.php
+++ src/php/partial/nav.php
@@ -4,9 +4,8 @@
         <div>
             <?php if (BACK): ?><a class="sidebar-backlink" href="<?= BACK ?>">Back</a></a><?php endif ?>
             <?php if (defined('BLEND_NAME')): ?>
-                <?php $packages = Package::rgetAll(); ?>
-                <?php $current_package = null; ?>
-                <?php if (count($packages) > 1): ?>
+                <?php $nav = @Config::get()->nav ?? []; ?>
+                <?php if (count($nav) > 1): ?>
                     <?php
                         $query = implode('&', array_map(function($v, $k) { return "{$k}={$v}"; }, $_GET, array_keys($_GET)));
                         $query = $query ? '?' . $query : '';
@@ -14,35 +13,38 @@
                     <div class="navset">
                         <div class="inline-modal">
                             <div class="nav-dropdown">
-                                <?php foreach ($packages as $alias => $package): ?>
-                                    <?php $blend = $blend_lookup[$package->blends[0]]; ?>
-                                    <?php
-                                        $iscurrentpackage = false;
-
-                                        if (in_array(BLEND_NAME, $package->blends)) {
-                                            $current_package_alias = $alias;
-                                            $current_package = $package;
-                                            $iscurrentpackage = true;
-                                        }
-                                    ?>
-                                    <a href="/<?= $alias ?>/blend/<?= $blend->name ?><?= $query ?>" <?= $iscurrentpackage ? 'class="current"' : ''?>><?= @$package->label ?? $alias ?></a>
+                                <?php
+                                    $ddlabel = null;
+                                    $current_navgroup = null;
+                                    $current_navgroup_items = [];
+                                ?>
+                                <?php foreach ($nav as $key => $value): ?>
+                                    <?php if (is_string($key)): ?>
+                                        <?php $blend = $blend_lookup[$value[0]]; ?>
+                                        <?php if (in_array(BLEND_NAME, $value)) { $current_navgroup = $key; $current_navgroup_items = $value; $ddlabel = $current_navgroup; } ?>
+                                        <a href="/blend/<?= $blend->name ?><?= $query ?>" <?= $current_navgroup == $key ? 'class="current"' : '' ?>><?= $key ?></a>
+                                    <?php else: ?>
+                                        <?php $blend = $blend_lookup[$value]; ?>
+                                        <?php if ($blend->name == BLEND_NAME) { $ddlabel = @$blend->label ?? $blend->name; } ?>
+                                        <a href="/blend/<?= $blend->name ?><?= $query ?>" <?= $blend->name == BLEND_NAME ? 'class="current"' : '' ?>><?= @$blend->label ?? $blend->name ?></a>
+                                    <?php endif ?>
                                 <?php endforeach ?>
                             </div>
                         </div>
-                        <span class="inline-modal-trigger"><?= @$current_package->label ?? $current_package->name ?></span>
+                        <span class="inline-modal-trigger"><?= $ddlabel ?></span>
                     </div>
 
-                    <?php if (count($current_package->blends) > 1): ?>
+                    <?php if (count($current_navgroup_items) > 1): ?>
                         <div class="navset">
                             <div class="inline-modal">
                                 <div class="nav-dropdown">
-                                    <?php foreach ($current_package->blends as $blend_name): ?>
-                                        <?php $blend = $blend_lookup[$blend_name]; ?>
-                                        <a href="/<?= $current_package_alias ?>/blend/<?= $blend->name ?><?= $query ?>" <?= $blend->name == BLEND_NAME ? 'class="current"' : ''?>><?= @$blend->label ?? $blend_name ?></a>
+                                    <?php foreach ($current_navgroup_items as $value): ?>
+                                        <?php $blend = $blend_lookup[$value]; ?>
+                                        <a href="/blend/<?= $blend->name ?><?= $query ?>" <?= $blend->name == BLEND_NAME ? 'class="current"' : '' ?>><?= @$blend->label ?? $blend->name ?></a>
                                     <?php endforeach ?>
                                 </div>
                             </div>
-                            <span class="inline-modal-trigger"><?= $blend_lookup[BLEND_NAME]->label ?></span>
+                            <span class="inline-modal-trigger"><?= @Blend::load(BLEND_NAME)->label ?? BLEND_NAME ?></span>
                         </div>
                     <?php endif ?>
                 <?php endif ?>
