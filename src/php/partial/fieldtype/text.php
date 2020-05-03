0564b1eb28263e8d1d594d0bb69e374da3241ab8
Oran Fry <oran@emerg.co>
2020-05-03 12:23:39 +1200

    Allow custom labels to select elements

--- src/php/partial/fieldtype/text.php
+++ src/php/partial/fieldtype/text.php
@@ -1,8 +1,12 @@
 <?php if ($options): ?>
     <select name="<?= $field->name ?>" style="width: 80%" tabindex="1">
         <?php if (!@$field->constrained || count($options) > 1): ?><option></option><?php endif ?>
-        <?php foreach ($options as $_value): ?>
-            <option <?= $_value == @$value ? 'selected="selected"' : '' ?>><?= $_value ?></option>
+        <?php foreach ($options as $k => $v): ?>
+            <?php
+                $_value = $v;
+                $_label = is_numeric($k) ? $v : $k;
+            ?>
+            <option <?= $_value == @$value ? 'selected="selected"' : '' ?> value="<?= $_value ?>"><?= $_label ?></option>
         <?php endforeach ?>
     </select>
     <?php if (!@$field->constrained): ?>
