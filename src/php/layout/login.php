9cfabfbc878a87d77938c51b7ce2be674e0f77e9
Oran Fry <oran@emerg.co>
2020-07-06 14:09:59 +1200

    Whitespace

--- src/php/layout/login.php
+++ src/php/layout/login.php
@@ -18,26 +18,26 @@
                 </div>
                 <div class="col">
                     <form action="" method="post">
-                            <div style="display: inline-block; text-align: left; margin: 1em 0">
-                            <h1 class="desktop-only"><?= Config::get()->instance_name ?: 'Blends' ?></h1>
-                                <div class="cred-line">
-                                    <p>Username</p>
-                                    <input type="text" name="username" id="auth" autocomplete="off" value="<?= @$username ?>">
-                                </div>
-                                <div class="cred-line">
-                                    <p>Password</p>
-                                    <input type="password" name="password">
-                                </div>
-                                <div class="cred-line">
-                                    <input type="submit" value="Sign In">
-                                </div>
-
-                                <?php if (isset($message) && $message): ?>
-                                <div class="cred-line">
-                                    <p class="error"><?= $message ?></p>
-                                </div>
-                                <?php endif ?>
+                        <div style="display: inline-block; text-align: left; margin: 1em 0">
+                        <h1 class="desktop-only"><?= Config::get()->instance_name ?: 'Blends' ?></h1>
+                            <div class="cred-line">
+                                <p>Username</p>
+                                <input type="text" name="username" id="auth" autocomplete="off" value="<?= @$username ?>">
                             </div>
+                            <div class="cred-line">
+                                <p>Password</p>
+                                <input type="password" name="password">
+                            </div>
+                            <div class="cred-line">
+                                <input type="submit" value="Sign In">
+                            </div>
+
+                            <?php if (isset($message) && $message): ?>
+                            <div class="cred-line">
+                                <p class="error"><?= $message ?></p>
+                            </div>
+                            <?php endif ?>
+                        </div>
                     </form>
                 </div>
             </div>
